7134a5c41453721afc477ab452c389bd0f196228
Oscar <oarbman@kth.se>
2025-09-22 14:55:09 +0200

    test

--- __main__.py
+++ __main__.py
@@ -1,5 +1,6 @@
 from pulumi.provider.experimental import component_provider_host
 from staticpage import StaticPage
+from html_helper import TemplateRenderer
 
 if __name__ == "__main__":
-    component_provider_host(name="static-page-component", components=[StaticPage])
+    component_provider_host(name="static-page-component", components=[StaticPage, TemplateRenderer])
